76abe3ca7945164199a22e765a3c7c7a84f23d2a
breno_mg10@hotmail.com <breno_mg10@hotmail.com>
2025-01-28 20:14:05 -0300

    fix: ensure opacity affects only background image in styles.ts

--- src/styles.ts
+++ src/styles.ts
@@ -1,14 +1,27 @@
 import styled from "styled-components";
 
-export const Body = styled.body`
+export const Body = styled.div`
   width: 100%;
   height: 100vh;
-  background-image: url("/src/assets/books.jpg");
-  background-repeat: repeat;
-  background-size: auto;
-  background-position: top left;
-  animation: moveBackground 10s linear infinite;
-  opacity: 0.2;
+  position: relative;
+  overflow: hidden;
+
+  &::before {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-image: url("/src/assets/books.jpg");
+    background-repeat: repeat;
+    background-size: auto;
+    background-position: top left;
+    animation: moveBackground 10s linear infinite;
+    opacity: 0.2;
+    z-index: -1;
+  }
+
   @keyframes moveBackground {
     from {
       background-position: top left;
