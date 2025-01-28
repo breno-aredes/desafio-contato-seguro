6af2bd661a18b1b5e47a6368f802a7cf0400ea45
breno_mg10@hotmail.com <breno_mg10@hotmail.com>
2025-01-28 20:44:14 -0300

    feat: create header component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import * as S from "./styles";
+
+const Header: React.FC = () => {
+  return (
+    <S.HeaderContainer>
+      <S.Title>Definir um nome</S.Title>
+    </S.HeaderContainer>
+  );
+};
+
+export default Header;
---
+++ src/components/Header/styles.ts
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const HeaderContainer = styled.header`
+  background-color: #f8f9fa;
+  padding: 20px;
+  text-align: center;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+  margin-bottom: 20px;
+`;
+
+export const Title = styled.h1`
+  margin: 0;
+  font-size: 24px;
+  color: #333;
+`;
